8e53bce129ae158ed5b248d8ddfd3a447731dc79
tali-creator <talinanzing111@gmail.com>
2024-11-24 09:20:08 +0100

    Default App.js setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <div className="w-fit bg-primary mx-auto my-[200px] p-5 rounded-lg">
+    <div className="w-full h-screen ">
+    <div className="w-full md:w-5/6 py-5 px-5 md:px-16 bg-primary mx-auto h-full">
       <img src="/fitech-logo.png" className="w-[100px] h-[100px]" alt="" />
       <h1 className="text-3xl font-black w-fit text-center text-white">Fitech</h1>
+    </div>
     </div>
   )
 }
