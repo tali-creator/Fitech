8fa3130481be1e7c29ec34781699da1cd9cd5a99
tali-creator <talinanzing111@gmail.com>
2024-11-24 09:30:13 +0100

    component assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div className="w-full h-screen ">
-    <div className="w-full md:w-5/6 py-5 px-5 md:px-16 bg-primary mx-auto h-full">
-      <img src="/fitech-logo.png" className="w-[100px] h-[100px]" alt="" />
-      <h1 className="text-3xl font-black w-fit text-center text-white">Fitech</h1>
+      <div className="w-full md:w-5/6 py-5 px-5 md:px-16 bg-primary mx-auto h-full">
+        {/* <img src="/fitech-logo.png" className="w-[100px] h-[100px]" alt="" />
+        <h1 className="text-3xl font-black w-fit text-center text-white">
+          Fitech
+        </h1> */}
+
+        {/* Chuku obesd and Tali content goes under here */}
+
+
+          {/* Aisha Kabir content goes under here */}
+
+
+          {/* Ruben Luka content goes under here */}
+
+
+          {/* Taibat Olayinka content under goes here */}
+
+
+          {/* Rufai Usman content goes under here */}
+
+
+          {/* Jethro irmiya content goes under here */}
+
+
+          {/* Fatima's content goes under here */}
+
+
+          {/* Tali Nanzing Moses content under goes here */}
+
+
+          {/*Chukwu Obed content goes under here */}
+
+          
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
