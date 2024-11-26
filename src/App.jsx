f4dbb9b5e2a2abf0f0f00601f7fb0e31c5a96fe8
tali-creator <talinanzing111@gmail.com>
2024-11-26 09:38:48 +0100

    Tali Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Testimonies from "./tali/Testimonies";
 import Details from "./tali/details";
+import Dashboard from "./dashboard";
 
 function App() {
   return (
     <div className="w-full h-auto ">
       <div className="w-full py-5 px-5 md:px-16 bg-primary mx-auto h-full">
-        {/* <img src="/fitech-logo.png" className="w-[100px] h-[100px]" alt="" />
-        <h1 className="text-3xl font-black w-fit text-center text-white">
-          Fitech
-        </h1> */}
+        {/* Tali and Chukwu obed code goes  under here */}
         <Routes>
-          {/* Chuku obesd and Tali content goes under here */}
-
-          {/* Aisha Kabir content goes under here */}
-
-          {/* Ruben Luka content goes under here */}
-
-          {/* Taibat Olayinka content under goes here */}
-
-          {/* Rufai Usman content goes under here */}
-
-          {/* Jethro irmiya content goes under here */}
-
-          {/* Fatima's content goes under here */}
-
-          {/* Tali Nanzing Moses content under goes here */}
-          <Route path="/" element={<Testimonies />} />
-          {/*Chukwu Obed content goes under here */}
-
+          <Route path="/" element={<Dashboard />} />
           <Route path="/user/:id" element={<Details />} />
         </Routes>
+        {/*Chukwu Obed content goes under here */}
       </div>
     </div>
   );
---
+++ src/dashboard.jsx
@@ -0,0 +1,24 @@
+import Testimonies from "./tali/Testimonies";
+
+export default function Dashboard() {
+  return (
+    <div>
+      {/* Chuku obesd and Tali content goes under here */}
+
+      {/* Aisha Kabir content goes under here */}
+
+      {/* Ruben Luka content goes under here */}
+
+      {/* Taibat Olayinka content under goes here */}
+
+      {/* Rufai Usman content goes under here */}
+
+      {/* Jethro irmiya content goes under here */}
+
+      {/* Fatima's content goes under here */}
+
+      {/* Tali Nanzing Moses content under goes here */}
+      <Testimonies />
+    </div>
+  );
+}
