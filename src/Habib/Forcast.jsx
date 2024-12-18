47ed8ae611f01c2e665f21a882ac95b3c3cf426f
tali-creator <talinanzing111@gmail.com>
2024-12-18 11:04:50 +0100

    tali updated

--- src/Habib/Forcast.jsx
+++ src/Habib/Forcast.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-
 const Forcast = () => {
   return (
-    <div className='forcast w-full h-auto p-[10px] flex flex-wrap '>
-      <div className='sub-forcast w-full relative min-h-[602px] flex flex-row justify-center  '>
-        <div>
-          <div className='absolute top-[60px] right-[130px] left-[3px] '><img className='w-[210px]' src="/iPhone12Pro1.png" alt="iphone 12 pro one" /></div>
-          <div className='absolute top-[130px] left-[120px] z-10'><img className='w-[210px]' src="/iPhone12Pro.png" alt="iphone 12 pro one" /></div>
+    <div className="w-full flex flex-col md:flex-row space-y-10 md:space-y-0">
+      <div className="w-full flex justify-center relative">
+        <div className="absolute w-full h-full border bg-blue-400/10 blur-xl rounded-[200px] border-red-50"></div>
+        <div className="flex w-full z-90 justify-center relative -space-x-20 max-h-[600px]">
+          <img
+            className="pb-20 md:w-[300px] min-w-[150px] "
+            src="/iPhone12Pro.png"
+            alt=""
+          />
+          <img
+            className="pt-20 md:w-[300px] min-w-[150px] "
+            src="/iPhone12Pro1.png"
+            alt=""
+          />
         </div>
-        
       </div>
-      <div className='text-white'>
-        <h3 className='font-bold text-[#962F79] text-[25px] '>How it works</h3>
-        <h1 className='text-[60px] font-medium my-[30px] ' >Viewing Long-term and Short-term forcast</h1>
-        <p className=' text-[#767799] text-[18px] my-[80px] '>Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut voluptatum labore et possimus voluptas.</p>
-        <button className='border-[2px] w-[200px] h-[60px] '>Explore Now</button>
+      <div className="flex flex-col w-full space-y-10 md:pt-17 md:pl-5">
+        <h1 className="text-tertiary text-2xl ">How it works</h1>
+        <h1 className=" text-3xl sm:text-5xl text-white md:text-6xl font-black ">
+          Viewing Long-term and Short-term forcast
+        </h1>
+        <p className="text-secondary text-lg md:w-4/5">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id aut non
+          cupiditate consectetur dolores. Dolorum enim possimus fuga placeat
+          asperiores nihil ipsam suscipit magnam dolores!
+        </p>
+        <div className="w-full ">
+          <button className="text-white text-2xl font-black border-2 border-white py-2 px-5 ">
+            Explore Now
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Forcast
+export default Forcast;
--- src/tali and obed/links.jsx
+++ src/tali and obed/links.jsx
@@ -16,100 +16,81 @@ export default function Links() {
 
   return (
     <div>
-    <div className="text-secondary flex space-x-4 justify-center items-center">
-      <div className="sm:flex space-x-6 hidden text-md">
-        <NavLink
-          className=" transition-colors duration-300 hover:text-white"
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className=" transition-colors duration-300 hover:text-white"
-          to="/about"
-        >
-          About
-        </NavLink>
-        <NavLink
-          className=" transition-colors duration-300 hover:text-white"
-          to="/service"
-        >
-          Service
-        </NavLink>
-        <NavLink
-          className=" transition-colors duration-300 hover:text-white"
-          to="/event"
-        >
-          Events
-        </NavLink>
-      </div>
+      <div className="text-secondary flex space-x-4 justify-center items-center ">
+        <div className="sm:flex space-x-6 hidden text-md">
+          <NavLink
+            className=" transition-colors duration-300 hover:text-white"
+            to="/">
+            Home
+          </NavLink>
+          <NavLink
+            className=" transition-colors duration-300 hover:text-white"
+            to="/about">
+            About
+          </NavLink>
+          <NavLink
+            className=" transition-colors duration-300 hover:text-white"
+            to="/service">
+            Service
+          </NavLink>
+          <NavLink
+            className=" transition-colors duration-300 hover:text-white"
+            to="/event">
+            Events
+          </NavLink>
+        </div>
 
-      <div
-        style={{
-          position: toggle ? "absolute" : "",
-          backgroundColor: toggle ? "rgba(18, 19, 42, 1)" : "",
-        }}
-        className="top-0 left-0 w-full transition ease-linear duration-300"
-      >
-        {" "}
-        <div className="w-full flex justify-end p-3 cursor-pointer ">
-          <div
-            style={{ padding: toggle ? "10px" : "" }}
-            onClick={handleClick}
-            className="space-y-1 sm:hidden w-fit"
-          >
+        <div
+          style={{
+            position: toggle ? "absolute" : "",
+            backgroundColor: toggle ? "rgba(18, 19, 42, 1)" : "",
+          }}
+          className="top-0 left-0 w-full transition ease-linear duration-300">
+          <div className="w-full flex justify-end p-3 cursor-pointer ">
             <div
-              className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
-                toggle && "-rotate-45 translate-y-2"
-              }`}
-            ></div>
-            <div
-              
-              className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${toggle && "hidden"}`}></div>
-            <div      
-             
-              className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${toggle && "rotate-45"}`}></div>
-              className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
-                toggle && "hidden"
-              }`}
+              style={{ padding: toggle ? "10px" : "" }}
+              onClick={handleClick}
+              className="space-y-1 sm:hidden w-fit">
+              <div
+                className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
+                  toggle && "-rotate-45 translate-y-2"
+                }`}></div>
+              <div
+                className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
+                  toggle && "hidden"
+                }`}></div>
+              <div
+                className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
+                  toggle && "rotate-45"
+                }`}></div>
             </div>
-            <div
-              className={`w-7 h-1 transform transition-all hover:bg-white duration-300 bg-secondary rounded ${
-                toggle && "rotate-45"
-              }`}
-            ></div>
           </div>
         </div>
         <div
           style={{ display: `${toggle ? "flex" : "none"}` }}
-          className=" flex-col w-full justify-start items-start px-10"
-        >
+          className=" flex-col w-full justify-start items-start px-10">
           <NavLink
             onClick={handleClick}
             className=" p-2 w-full hover:border-b-2 transition-all duration-200 hover:text-white"
-            to="/"
-          >
+            to="/">
             Home
           </NavLink>
           <NavLink
             onClick={handleClick}
             className=" p-2 w-full hover:border-b-2 transition-all duration-200 hover:text-white"
-            to="/about"
-          >
+            to="/about">
             About
           </NavLink>
           <NavLink
             onClick={handleClick}
             className=" p-2 w-full hover:border-b-2 transition-all duration-200 hover:text-white"
-            to="/service"
-          >
+            to="/service">
             Service
           </NavLink>
           <NavLink
             onClick={handleClick}
             className=" p-2 w-full hover:border-b-2 transition-all duration-200 hover:text-white"
-            to="/event"
-          >
+            to="/event">
             Events
           </NavLink>
 
@@ -117,11 +98,9 @@ export default function Links() {
             {!user ? (
               <>
                 <NavLink className="flex flex-none " to={"/login"}>
-                  {" "}
                   <Button button={"Login"} />
                 </NavLink>
                 <NavLink className="flex flex-none" to={"/signIn"}>
-                  {" "}
                   <Button button={"Sign in"} />
                 </NavLink>
               </>
@@ -132,25 +111,22 @@ export default function Links() {
             )}
           </div>
         </div>
-      </div>
-
-      <div className="hidden sm:flex flex-none space-x-4 pr-2">
-        {!user ? (
-          <>
-            <NavLink className="flex flex-none " to={"/login"}>
-              {" "}
-              <Button button={"Login"} />
-            </NavLink>
-            <NavLink className="flex flex-none" to={"/signIn"}>
-              {" "}
-              <Button button={"Sign in"} />
-            </NavLink>
-          </>
-        ) : (
-          <button className="cursor-pointer" onClick={LogOut}>
-            <Button button={"LogOut"} />
-          </button>
-        )}
+        <div className="hidden sm:flex flex-none space-x-4 pr-2 ">
+          {!user ? (
+            <>
+              <NavLink className="flex flex-none " to={"/login"}>
+                <Button button={"Login"} />
+              </NavLink>
+              <NavLink className="flex flex-none" to={"/signIn"}>
+                <Button button={"Sign in"} />
+              </NavLink>
+            </>
+          ) : (
+            <button className="cursor-pointer" onClick={LogOut}>
+              <Button button={"LogOut"} />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
